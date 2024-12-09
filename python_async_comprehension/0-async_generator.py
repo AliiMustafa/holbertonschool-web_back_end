4a534c1cfac87c971a29ebbfce2dfc96d27f47ea
AliiMustafa <aliimustafazada.com>
2024-12-09 19:04:33 +0400

    Python asynchronous comprehension

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Function for generator"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
